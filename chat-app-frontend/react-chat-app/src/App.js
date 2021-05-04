ba00359c4da3f45c2ae2be9f1281a9f69599d51c
Drew Hemsley <drew.hemsleygetweave.com@drews-mbp-2.lan>
2021-05-03 20:25:25 -0600

    made state changes

--- chat-app-frontend/react-chat-app/src/App.js
+++ chat-app-frontend/react-chat-app/src/App.js
@@ -6,25 +6,56 @@ class App extends Component {
     super(props);
     this.state = {
       name: "",
+      timeOnLoad: "",
+      currentDate: "",
     };
   }
+  componentDidMount() {
+    var d = new Date().toString();
+    this.setState({
+      currentDate: d,
+    });
+  }
 
-  handleClick = (word) => {
-    alert(word);
+  updateTime = () => {
+    this.setState({
+      timeOnLoad: new Date(),
+    });
   };
+
+  handleClick = () => {
+    alert(this.state.name);
+  };
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.name);
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div className="App">
-        <div className="chat_box">
-          <button onClick={() => this.handleClick("Drew")}>
+        <div className="chat_box_main">
+          <form onSubmit={() => this.handleSubmit()}>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={(event) => this.handleChange(event)}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <button onClick={() => this.handleClick()}>
             YOU KNOW WHAT IT IS
           </button>
-        </div>
-        <div className="chat_box">
-          <button onClick={() => this.handleClick("Drew")}>
-            YOU KNOW WHAT IT IS
-          </button>
+          <div>{this.state.currentDate} - Last load timestamp</div>
+          <div>{this.state.name} - This is the current name in state</div>
         </div>
       </div>
     );
